e828213e7cae84370559efc8659e2efe0033a211
persikoloid <dimasmiryagin@gmail.com>
2021-09-23 01:05:51 +0300

    Second

---
+++ Assets/Test Task/Scripts/ChooseObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Test_Task.Scripts
+{
+    public class ChooseObject : MonoBehaviour
+    {
+        [Header("Объекты для спавна на экране")]
+        [SerializeField]
+        private GameObject objectForSpawn;
+        private Manager _manager;
+
+        private void Start()
+        {
+            _manager = FindObjectOfType<Manager>();
+            GetComponent<Button>().onClick.AddListener(SetObject);
+        }
+
+        private void SetObject()
+        {
+            _manager.objectForSpawn = objectForSpawn;
+        }
+    }
+}
